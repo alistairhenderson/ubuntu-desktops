4ff620cb9192e2b6f0ed71269f38cee8e48a2a26
Alistair Henderson <al@alistairhenderson.com>
2023-01-27 08:52:54 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-ip_address=$1
-subnet=$2
-gateway=$3
-DNS_1=$4
-DNS_2=$5
+network_setup=$1
+ip=$2
+subnet=$3
+gateway=$4
+DNS_1=$5
+DNS_2=$6
+domain=$7
 
-
-if [ -z "$var" ]; then 
+if [ -z "$ip" ]; then 
 apt install -y openssh-server
 
 else
+
+
 
 
 apt update
@@ -21,4 +24,22 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 
 
+fi
+
+
+if [ "$network_setup" == "Y" ]; then
+# This is the network config written by 'subiquity'
+network:
+  ethernets:
+    wlp1s0:
+            #dhcp4: true
+            addresses:
+                - $ip/$subnet
+            routes:
+                - to: default
+                  via: $gateway
+            nameservers:
+                  search: [$domain]
+                  addresses: [$dns1, $dns2]
+  version: 2
 fi
